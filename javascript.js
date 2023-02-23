da78bee4652e3717588a6a7c3c800abda31c9d73
JoeIbo1 <josef5222@hotmail.com>
2023-02-23 10:51:32 +0100

    second semi update

--- javascript.js
+++ javascript.js
@@ -1,23 +1,37 @@
+"use strict";
+
 window.addEventListener("load", start);
+
+let lives = 3;
 
 function start() {
   // Start animationer
   document.querySelector("#rocket_container").classList.add("falling");
   document.querySelector("#rocket_container2").classList.add("falling2");
   document.querySelector("#astroid_container").classList.add("astro");
+  document.querySelector("#astroid_container2").classList.add("astro2");
   // Registrer click
-  document.querySelector("#rocket_container").addEventListener("click", clickRocket);
-  document.querySelector("#rocket_container2").addEventListener("click", clickRocket2);
-    document.querySelector("#astroid_container").addEventListener("click", clickAstroid);
-
+  document
+    .querySelector("#rocket_container")
+    .addEventListener("click", clickRocket);
+  document
+    .querySelector("#rocket_container2")
+    .addEventListener("click", clickRocket2);
+  document
+    .querySelector("#astroid_container")
+    .addEventListener("click", clickAstroid);
+  document
+    .querySelector("#astroid_container2")
+    .addEventListener("click", clickAstroid2);
 }
 
-
 function clickRocket() {
-  console.log("Click coin");
+  console.log("Click rocket");
   // Forhindr gentagne clicks
-  document.querySelector("#rocket_container").removeEventListener("click", clickRocket);
-  
+  document
+    .querySelector("#rocket_container")
+    .removeEventListener("click", clickRocket);
+
   // Stop coin container
   document.querySelector("#rocket_container").classList.add("paused");
 
@@ -25,13 +39,17 @@ function clickRocket() {
   document.querySelector("#rocket_sprite").classList.add("zoom_out");
 
   // når forsvind-animation er færdig: coinGone
-  document.querySelector("#rocket_container").addEventListener("animationend", rocketGone);
+  document
+    .querySelector("#rocket_container")
+    .addEventListener("animationend", rocketGone);
 }
 function clickRocket2() {
-  console.log("Click coin");
+  console.log("Click rocket2");
   // Forhindr gentagne clicks
-  document.querySelector("#rocket_container2").removeEventListener("click", clickRocket2);
-  
+  document
+    .querySelector("#rocket_container2")
+    .removeEventListener("click", clickRocket2);
+
   // Stop coin container
   document.querySelector("#rocket_container2").classList.add("paused");
 
@@ -39,13 +57,17 @@ function clickRocket2() {
   document.querySelector("#rocket_sprite2").classList.add("zoom_out");
 
   // når forsvind-animation er færdig: coinGone
-  document.querySelector("#rocket_container2").addEventListener("animationend", rocketGone2);
+  document
+    .querySelector("#rocket_container2")
+    .addEventListener("animationend", rocketGone2);
 }
 
 function clickAstroid() {
-  console.log("Click coin");
+  console.log("Click astroid");
   // Forhindr gentagne clicks
-  document.querySelector("#astroid_container").removeEventListener("click", clickAstroid);
+  document
+    .querySelector("#astroid_container")
+    .removeEventListener("click", clickAstroid);
 
   // Stop coin container
   document.querySelector("#astroid_container").classList.add("paused");
@@ -54,17 +76,34 @@ function clickAstroid() {
   document.querySelector("#astroid_sprite").classList.add("zoom_in");
 
   // når forsvind-animation er færdig: coinGone
-  document.querySelector("#astroid_container").addEventListener("animationend", astroidGone);
+  document
+    .querySelector("#astroid_container")
+    .addEventListener("animationend", astroidGone);
 }
+function clickAstroid2() {
+  console.log("Click astroid2");
+  // Forhindr gentagne clicks
+  document
+    .querySelector("#astroid_container2")
+    .removeEventListener("click", clickAstroid2);
 
+  // Stop coin container
+  document.querySelector("#astroid_container2").classList.add("paused");
 
+  // sæt forsvind-animation på coin
+  document.querySelector("#astroid_sprite2").classList.add("zoom_in");
 
-
-
+  // når forsvind-animation er færdig: coinGone
+  document
+    .querySelector("#astroid_container2")
+    .addEventListener("animationend", astroidGone2);
+}
 
 function rocketGone() {
   // fjern event der bringer os herind
-  document.querySelector("#rocket_container").removeEventListener("animationend", rocketGone);
+  document
+    .querySelector("#rocket_container")
+    .removeEventListener("animationend", rocketGone);
 
   // fjern forsvind-animation
   document.querySelector("#rocket_sprite").classList.remove("zoom_out");
@@ -78,11 +117,15 @@ function rocketGone() {
   document.querySelector("#rocket_container").classList.add("falling");
 
   // gør det muligt at klikke på coin igen
-  document.querySelector("#rocket_container").addEventListener("click", clickRocket);
+  document
+    .querySelector("#rocket_container")
+    .addEventListener("click", clickRocket);
 }
 function rocketGone2() {
   // fjern event der bringer os herind
-  document.querySelector("#rocket_container2").removeEventListener("animationend", rocketGone2);
+  document
+    .querySelector("#rocket_container2")
+    .removeEventListener("animationend", rocketGone2);
 
   // fjern forsvind-animation
   document.querySelector("#rocket_sprite2").classList.remove("zoom_out");
@@ -96,13 +139,16 @@ function rocketGone2() {
   document.querySelector("#rocket_container2").classList.add("falling2");
 
   // gør det muligt at klikke på coin igen
-  document.querySelector("#rocket_container2").addEventListener("click", clickRocket2);
+  document
+    .querySelector("#rocket_container2")
+    .addEventListener("click", clickRocket2);
 }
-
 
 function astroidGone() {
   // fjern event der bringer os herind
-  document.querySelector("#astroid_container").removeEventListener("animationend", astroidGone);
+  document
+    .querySelector("#astroid_container")
+    .removeEventListener("animationend", astroidGone);
 
   // fjern forsvind-animation
   document.querySelector("#astroid_sprite").classList.remove("zoom_in");
@@ -116,5 +162,29 @@ function astroidGone() {
   document.querySelector("#astroid_container").classList.add("astro");
 
   // gør det muligt at klikke på coin igen
-  document.querySelector("#astroid_container").addEventListener("click", clickAstroid);
+  document
+    .querySelector("#astroid_container")
+    .addEventListener("click", clickAstroid);
+}
+function astroidGone2() {
+  // fjern event der bringer os herind
+  document
+    .querySelector("#astroid_container2")
+    .removeEventListener("animationend", astroidGone2);
+
+  // fjern forsvind-animation
+  document.querySelector("#astroid_sprite2").classList.remove("zoom_in");
+
+  // fjern pause
+  document.querySelector("#astroid_container2").classList.remove("paused");
+
+  // genstart falling animation
+  document.querySelector("#astroid_container2").classList.remove("astro2");
+  document.querySelector("#astroid_container2").offsetWidth;
+  document.querySelector("#astroid_container2").classList.add("astro2");
+
+  // gør det muligt at klikke på coin igen
+  document
+    .querySelector("#astroid_container2")
+    .addEventListener("click", clickAstroid2);
 }
