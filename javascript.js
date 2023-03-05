c47696b7c33d92f64964be663ba387c86f83003d
JoeIbo1 <josef5222@hotmail.com>
2023-03-05 20:40:28 +0100

    soon finish

--- javascript.js
+++ javascript.js
@@ -1,21 +1,34 @@
 "use strict";
 
-window.addEventListener("load", start);
+window.addEventListener("load", ready);
 
+let animationTime = 3000;
 let lives = 6;
 let points = 0;
+
+function ready() {
+  console.log("JavaScript ready!");
+  document.querySelector("#btn_start").addEventListener("click", start);
+  document.querySelector("#btn_go_to_start").addEventListener("click", showStartScreen);
+}
+
 function start() {
+  document.querySelector("#start").classList.add("hidden");
+
+  document.querySelector("#play_game").play();
 
   animationstart();
   positionStart();
   registerClick();
+  // start timer
+  startTimer();
+  setTimeout(checkAnimations, animationTime);
+
 
   document.querySelector("#meteor_container1").addEventListener("animationiteration", restartFalling);
   document.querySelector("#meteor_container2").addEventListener("animationiteration", restartFalling);
   document.querySelector("#meteor_container3").addEventListener("animationiteration", restartFalling);
   document.querySelector("#meteor_container4").addEventListener("animationiteration", restartFalling);
-
-  document.querySelector("#play_game").play();
 }
 
 function animationstart(){
@@ -52,9 +65,9 @@ function registerClick(){
 
   // Registrer click
   rocket.addEventListener("click", clickRocket);
-  rocket2.addEventListener("click", clickRocket2);
+  rocket2.addEventListener("click", clickRocket);
   astroid.addEventListener("click", clickAstroid);
-  astroid2.addEventListener("click", clickAstroid2);
+  astroid2.addEventListener("click", clickAstroid);
   meteor.addEventListener("click", clickMeteo);
   meteor2.addEventListener("click", clickMeteo);
   meteor3.addEventListener("click", clickMeteo);
@@ -68,19 +81,31 @@ function positionStart(){
   document.querySelector("#meteor_container4").classList.add("position4");
 }
 
+function checkAnimations() {
+  let animations = document.querySelectorAll(".falling, .astro, .meteoFalling");
+  animations.forEach((animation) => {
+    if (!animation.classList.contains("paused")) {
+      decrementLives();
+      animationGone.call(animation);
+    }
+  });
+}
+
+
 function clickRocket(){
   console.log("Click rocket");
+  let rock = this;
   // Forhindr gentagne clicks
-  document.querySelector("#rocket_container").removeEventListener("click", clickRocket);
+  rock.removeEventListener("click", clickRocket);
 
   // Stop coin container
-  document.querySelector("#rocket_container").classList.add("paused");
+  rock.classList.add("paused");
 
   // sæt forsvind-animation på coin
-  document.querySelector("#rocket_sprite").classList.add("zoom_out");
+  rock.querySelector("img").classList.add("zoom_out");
 
   // når forsvind-animation er færdig: coinGone
-  document.querySelector("#rocket_container").addEventListener("animationend", rocketGone);
+  rock.addEventListener("animationend", rocketGone);
 
     document.querySelector("#play_rocket").currentTime = 0;
     document.querySelector("#play_rocket").play();
@@ -88,56 +113,23 @@ function clickRocket(){
   incrementPoints5();
 }
 
-function clickRocket2() {
-  console.log("Click rocket2");
-  // Forhindr gentagne clicks
-  document.querySelector("#rocket_container2").removeEventListener("click", clickRocket2);
-
-  // Stop coin container
-  document.querySelector("#rocket_container2").classList.add("paused");
-
-  // sæt forsvind-animation på coin
-  document.querySelector("#rocket_sprite2").classList.add("zoom_out");
-
-  // når forsvind-animation er færdig: coinGone
-  document.querySelector("#rocket_container2").addEventListener("animationend", rocketGone2);
-
-  incrementPoints5();
-}
-
 function clickAstroid() {
   console.log("Click astroid");
+  let astro = this;
   // Forhindr gentagne clicks
-  document.querySelector("#astroid_container").removeEventListener("click", clickAstroid);
+  astro.removeEventListener("click", clickAstroid);
 
   // Stop coin container
-  document.querySelector("#astroid_container").classList.add("paused");
+astro.classList.add("paused");
 
   // sæt forsvind-animation på coin
-  document.querySelector("#astroid_sprite").classList.add("zoom_in");
+  astro.querySelector("img").classList.add("zoom_in");
 
   // når forsvind-animation er færdig: coinGone
-  document.querySelector("#astroid_container").addEventListener("animationend", astroidGone);
+  astro.addEventListener("animationend", astroidGone);
 
     document.querySelector("#play_astroid").currentTime = 0;
     document.querySelector("#play_astroid").play();
-
-  incrementPoints10();
-}
-
-function clickAstroid2() {
-  console.log("Click astroid2");
-  // Forhindr gentagne clicks
-  document.querySelector("#astroid_container2").removeEventListener("click", clickAstroid2);
-
-  // Stop coin container
-  document.querySelector("#astroid_container2").classList.add("paused");
-
-  // sæt forsvind-animation på coin
-  document.querySelector("#astroid_sprite2").classList.add("zoom_in");
-
-  // når forsvind-animation er færdig: coinGone
-  document.querySelector("#astroid_container2").addEventListener("animationend", astroidGone2);
 
   incrementPoints10();
 }
@@ -148,95 +140,72 @@ function clickMeteo() {
   // Forhindr gentagne clicks
   meteor.removeEventListener("click", clickMeteo);
 
-  // Stop coin container
-  meteor.classList.add("paused");
+  // Start timer for animation
+  let timer = setTimeout(() => {
+    decrementLives();
+    meteorGone.call(meteor);
+  }, 3000);
 
-  // sæt forsvind-animation på coin
+  // Når forsvind-animation er færdig: coinGone
+  meteor.addEventListener("animationend", () => {
+    clearTimeout(timer);
+    meteorGone.call(meteor);
+  });
+
+  // Start the animation
+  meteor.classList.add("paused");
   meteor.querySelector("img").classList.add("zoom_m");
 
-  // når forsvind-animation er færdig: coinGone
-  meteor.addEventListener("animationend", meteorGone);
-
+  // Play the sound effect
   document.querySelector("#play_meto").currentTime = 0;
   document.querySelector("#play_meto").play();
 
-  incrementPoints();
+  // Increment points when the animation is clicked
+  meteor.addEventListener("click", () => {
+    incrementPoints();
+    clearTimeout(timer);
+    meteorGone.call(meteor);
+  });
 }
 
 function rocketGone() {
+  let rock = this;
   // fjern event der bringer os herind
-  document.querySelector("#rocket_container").removeEventListener("animationend", rocketGone);
+  rock.removeEventListener("animationend", rocketGone);
 
   // fjern forsvind-animation
-  document.querySelector("#rocket_sprite").classList.remove("zoom_out");
+  rock.querySelector("img").classList.remove("zoom_out");
 
   // fjern pause
-  document.querySelector("#rocket_container").classList.remove("paused");
+  rock.classList.remove("paused");
 
   // genstart falling animation
-  document.querySelector("#rocket_container").classList.remove("falling");
-  document.querySelector("#rocket_container").offsetWidth;
-  document.querySelector("#rocket_container").classList.add("falling");
+  rock.classList.remove("falling");
+  rock.offsetWidth;
+  rock.classList.add("falling");
 
   // gør det muligt at klikke på coin igen
-  document.querySelector("#rocket_container").addEventListener("click", clickRocket);
-}
-
-function rocketGone2() {
-  // fjern event der bringer os herind
-  document.querySelector("#rocket_container2").removeEventListener("animationend", rocketGone2);
-
-  // fjern forsvind-animation
-  document.querySelector("#rocket_sprite2").classList.remove("zoom_out");
-
-  // fjern pause
-  document.querySelector("#rocket_container2").classList.remove("paused");
-
-  // genstart falling animation
-  document.querySelector("#rocket_container2").classList.remove("falling2");
-  document.querySelector("#rocket_container2").offsetWidth;
-  document.querySelector("#rocket_container2").classList.add("falling2");
-
-  // gør det muligt at klikke på coin igen
-  document.querySelector("#rocket_container2").addEventListener("click", clickRocket2);
+  rock.addEventListener("click", clickRocket);
 }
 
 function astroidGone() {
+  let astro = this;
   // fjern event der bringer os herind
-  document.querySelector("#astroid_container").removeEventListener("animationend", astroidGone);
+  astro.removeEventListener("animationend", astroidGone);
 
   // fjern forsvind-animation
-  document.querySelector("#astroid_sprite").classList.remove("zoom_in");
+  astro.querySelector("img").classList.remove("zoom_in");
 
   // fjern pause
-  document.querySelector("#astroid_container").classList.remove("paused");
+  astro.classList.remove("paused");
 
   // genstart falling animation
-  document.querySelector("#astroid_container").classList.remove("astro");
-  document.querySelector("#astroid_container").offsetWidth;
-  document.querySelector("#astroid_container").classList.add("astro");
+  astro.classList.remove("astro");
+  astro.offsetWidth;
+  astro.classList.add("astro");
 
   // gør det muligt at klikke på coin igen
-  document.querySelector("#astroid_container").addEventListener("click", clickAstroid);
-}
-
-function astroidGone2() {
-  // fjern event der bringer os herind
-  document.querySelector("#astroid_container2").removeEventListener("animationend", astroidGone2);
-
-  // fjern forsvind-animation
-  document.querySelector("#astroid_sprite2").classList.remove("zoom_in");
-
-  // fjern pause
-  document.querySelector("#astroid_container2").classList.remove("paused");
-
-  // genstart falling animation
-  document.querySelector("#astroid_container2").classList.remove("astro2");
-  document.querySelector("#astroid_container2").offsetWidth;
-  document.querySelector("#astroid_container2").classList.add("astro2");
-
-  // gør det muligt at klikke på coin igen
-  document.querySelector("#astroid_container2").addEventListener("click", clickAstroid2);
+  astro.addEventListener("click", clickAstroid);
 }
 
 function meteorGone() {
@@ -251,9 +220,9 @@ function meteorGone() {
   meteo.classList.remove("paused");
 
   // genstart falling animation
-  meteo.classList.remove("meteo1");
+  meteo.classList.remove("meteoFalling");
   meteo.offsetWidth;
-  meteo.classList.add("meteo1");
+  meteo.classList.add("meteoFalling");
 
   // gør det muligt at klikke på coin igen
   meteo.addEventListener("click", clickMeteo);
@@ -264,6 +233,11 @@ function decrementLives() {
   showDecrementedLives();
   lives--;
   lose();
+}
+
+function showDecrementedLives() {
+  document.querySelector("#heart" + lives).classList.remove("active_heart");
+  document.querySelector("#heart" + lives).classList.add("broken_heart");
 }
 
 function lose() {
@@ -327,9 +301,61 @@ function restartFalling() {
   meteoer.classList.add("position" + pos);
 }
 
+function resetPoints() {
+  // nulstil point
+  points = 0;
+  // nulstil vising af point
+  displayPoints();
+}
+
+function resetLives() {
+  // sæt lives til 3
+  lives = 6;
+  //nulstil visning af liv (hjerte vi ser)
+  document.querySelector("#heart1").classList.remove("broken_heart");
+  document.querySelector("#heart2").classList.remove("broken_heart");
+  document.querySelector("#heart3").classList.remove("broken_heart");
+  document.querySelector("#heart4").classList.remove("broken_heart");
+  document.querySelector("#heart5").classList.remove("broken_heart");
+  document.querySelector("#heart6").classList.remove("broken_heart");
+  document.querySelector("#heart1").classList.add("active_heart");
+  document.querySelector("#heart2").classList.add("active_heart");
+  document.querySelector("#heart3").classList.add("active_heart");
+  document.querySelector("#heart4").classList.add("active_heart");
+  document.querySelector("#heart5").classList.add("active_heart");
+  document.querySelector("#heart6").classList.add("active_heart");
+}
+
+function showStartScreen() {
+  // fjern hidden fra startskærm og tilføj til game over og level complete
+  document.querySelector("#start").classList.remove("hidden");
+  document.querySelector("#game_over").classList.add("hidden");
+  document.querySelector("#level_complete").classList.add("hidden");
+}
+
+function startTimer() {
+  // Sæt timer-animationen (shrink) i gang ved at tilføje klassen shrink til time_sprite
+  document.querySelector("#time_sprite").classList.add("shrink");
+
+  // Tilføj en eventlistener som lytter efter at animationen er færdig (animationend) og kalder funktionen timeIsUp
+  document.querySelector("#time_sprite").addEventListener("animationend", timeIsUp);
+}
+
+function timeIsUp() {
+  console.log("Tiden er gået!");
+
+  if (points >= 50) {
+    win();
+  } else {
+    lose();
+  }
+}
+
 function stopGame() {
   // Stop animationer
-
+  resetLives();
+  resetPoints();
+console.log("stopped game");
   document.querySelector("#play_game").pause();
 
   document.querySelector("#meteor_container1").classList.remove("meteoFalling");
@@ -347,7 +373,22 @@ function stopGame() {
   document.querySelector("#meteor_container3").removeEventListener("click", clickMeteo);
   document.querySelector("#meteor_container4").removeEventListener("click", clickMeteo);
   document.querySelector("#astroid_container").removeEventListener("click", clickAstroid);
-  document.querySelector("#astroid_container2").removeEventListener("click", clickAstroid2);
+  document.querySelector("#astroid_container2").removeEventListener("click", clickAstroid);
   document.querySelector("#rocket_container").removeEventListener("click", clickRocket);
-  document.querySelector("#rocket_container2").removeEventListener("click", clickRocket2);
+  document.querySelector("#rocket_container2").removeEventListener("click", clickRocket);
+
+  document.querySelector("#meteor_container1").classList.remove("position1");
+  document.querySelector("#meteor_container2").classList.remove("position2");
+  document.querySelector("#meteor_container3").classList.remove("position3");
+  document.querySelector("#meteor_container4").classList.remove("position4");
+
+    // Stop og nulstil lyde, fx baggrundsmusik
+    document.querySelector("#sound_dreams").pause();
+    document.querySelector("#sound_dreams").currentTime = 0;
+
+    // nulstil timer - fjern animationen fra timeren (fjern klassen shrink fra time_sprite)
+    document.querySelector("#time_sprite").classList.remove("shrink");
+
 }
+
+
